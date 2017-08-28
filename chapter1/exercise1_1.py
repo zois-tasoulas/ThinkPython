847ae7b22110d86567607d19d2b03a13dd95583c
Zois Tasoulas <zoistas@hotmail.com>
2017-08-27 20:04:28 -0500

    Chapter 1 exercises

---
+++ chapter1/exercise1_1.py
@@ -0,0 +1,28 @@
+#Comment out the case you want to check. Maybe 
+#copying it to the interpreter is needed to notice
+#the outputs
+
+#Question 1
+#print('Hello, World!'
+
+#print'Hello, World!')
+
+#print 'Hello, World!'
+
+#Question 2
+#print(Hello, World!')
+
+#print('Hello, World!)
+
+#print(Hello, World!)
+
+#Question 3
+#2++4
+
+#++3 - 1
+
+#Question 4
+#05 + 3
+
+#Question 5
+#4 5
---
+++ chapter1/exercise1_2.py
@@ -0,0 +1,19 @@
+#Comment out the case you want to check. Maybe 
+#copying it to the interpreter is needed to notice
+#the outputs
+
+#Question 1
+#42 * 60 + 42
+
+#Question 2
+#10 / 1.61
+
+#Question 3
+#Pace in seconds
+#(10 / 1.61) / (42 * 60 + 42)
+
+#Pace in minutes
+#(10 / 1.61) / (42 + 42/60)
+
+#Speed in miles/h
+#(10 / 1.61) / (42 / 60 + (42 / 60) / 60)
